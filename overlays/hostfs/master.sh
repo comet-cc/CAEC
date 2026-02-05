510b7bacf6fbd652f9b83e3094d008a0389c9f30
SinaAb7 <sinaabdollahip@gmail.com>
2026-02-05 15:29:12 +0000

    fix typos

--- overlays/hostfs/master.sh
+++ overlays/hostfs/master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-sudo nice -n -20 taskset -c 0 ./lkvm run --realm -c 1 -m 500 -k /home/user/VM_image/Image -i /home/user/VM_image/VM-fs.cpio.gz \
+sudo nice -n -20 taskset -c 0 ./lkvm run --realm -c 1 -m 500 -k /home/user/VM_image/Image -i /home/user/VM_image/VM-fs.img \
 --9p /home/user/shared_with_VM,sh --irqchip=gicv3-its --loglevel=debug --restricted_mem -s 32 \
 --shmem 0x30000000:32M:file=/dev/shm/buf:create:mlock \
 -p "rdinit=/sbin/init console=ttyS0 cca_reserve=top,32M"  --dump-dtb=dtb.dtb \

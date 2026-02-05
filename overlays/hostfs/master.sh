3b05b09632dd0cdb470a17a1d3b46e47e23fa7a6
SinaAb7 <sinaabdollahip@gmail.com>
2026-02-05 13:58:17 +0000

    update overlay

--- overlays/hostfs/master.sh
+++ overlays/hostfs/master.sh
@@ -3,5 +3,5 @@ set -x
 sudo nice -n -20 taskset -c 0 ./lkvm run --realm -c 1 -m 500 -k /home/user/VM_image/Image -i /home/user/VM_image/VM-fs.cpio.gz \
 --9p /home/user/shared_with_VM,sh --irqchip=gicv3-its --loglevel=debug --restricted_mem -s 32 \
 --shmem 0x30000000:32M:file=/dev/shm/buf:create:mlock \
--p "rdinit=/sbin/init console=ttyS0"  --dump-dtb=dtb.dtb \
+-p "rdinit=/sbin/init console=ttyS0 cca_reserve=top,32M"  --dump-dtb=dtb.dtb \
 --pmu --pmu-counters 6
--- overlays/hostfs/slave.sh
+++ overlays/hostfs/slave.sh
@@ -3,4 +3,4 @@ set -x
 sudo nice -n -20 taskset -c 1 ./lkvm run --realm -c 1 -m 500 -k /home/user/VM_image/Image -i /home/user/VM_image/VM-fs.cpio.gz \
 --9p /home/user/shared_with_VM,sh --irqchip=gicv3-its --restricted_mem --loglevel=debug -r 32 \
 --shmem 0x30000000:32M:file=/dev/shm/buf:mlock \
--p "rdinit=/sbin/init console=ttyS0"
+-p "rdinit=/sbin/init console=ttyS0 cca_reserve=top,32M"
